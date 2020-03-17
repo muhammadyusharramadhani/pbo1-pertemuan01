9aa74ade2cc8f51352f0c2e99173945d941e391f
muhammadyuszhar <yushar1818@gmail.com>
2020-03-17 11:40:55 +0800

    Hello Git & java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
- public static vold main(String [] args){
-  System.out.println("Hello Word !!);
+ public static void main (String [] args){
+  System.out.println("Hello Git !!");
+  System.out.println("Hello Java !!");
  }
 }
